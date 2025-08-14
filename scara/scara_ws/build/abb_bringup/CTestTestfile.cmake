5b38d2521d222b1c47d17ab8c173aa30a2d01fdb
AFMC <andresfmc223@gmail.com>
2025-08-13 23:01:33 -0500

    Finished basic sims

---
+++ scara/scara_ws/build/abb_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/eia/repo/rbi_2025_2/scara/scara_ws/src/scara_gz_bringup
+# Build directory: /home/eia/repo/rbi_2025_2/scara/scara_ws/build/abb_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ scara/scara_ws/build/scara_gz_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/eia/repo/rbi_2025_2/scara/scara_ws/src/scara_gz_description
+# Build directory: /home/eia/repo/rbi_2025_2/scara/scara_ws/build/scara_gz_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
